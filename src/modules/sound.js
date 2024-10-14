d53e1c77896963fe99b1d9d9da3e129eba30e7e6
AlphaT7 <7742643+AlphaT7@users.noreply.github.com>
2024-10-14 15:19:28 -0400

    condensed the class code

--- src/modules/sound.js
+++ src/modules/sound.js
@@ -1,23 +1,20 @@
 export default class Sound {
-  constructor(sound) {
-    this.audio = fetch(sound.src);
-    this.audioBuffer;
-    this.volume = sound.volume;
+  constructor({ src, volume }) {
+    this.audio = fetch(src);
     this.audioCtx;
+    this.audioBuffer;
+    this.volume = volume;
   }
 
   async play() {
     let audio = await this.audio;
-    this.audioCtx = this.audioCtx == null ? new AudioContext() : this.audioCtx;
-    this.audioBuffer =
-      this.audioBuffer == null ? audio.arrayBuffer() : this.audioBuffer;
+    if (!this.audioCtx) this.audioCtx = new AudioContext();
+    if (!this.audioBuffer) this.audioBuffer = await audio.arrayBuffer();
 
     let audioCtx = this.audioCtx;
-    let audioBuffer = (await this.audioBuffer).slice();
-    let decodedAudio = await audioCtx.decodeAudioData(await audioBuffer);
     let sound = new AudioBufferSourceNode(audioCtx, { loop: false });
     let gainNode = audioCtx.createGain();
-    sound.buffer = await decodedAudio;
+    sound.buffer = await audioCtx.decodeAudioData(this.audioBuffer.slice());
     gainNode.gain.value = this.volume;
     sound.connect(gainNode).connect(audioCtx.destination);
     sound.start(0);
